b23df68554553ac0c822ebe8b4d9996391d1d915
yang491658 <park49164357@gmail.com>
2025-12-10 19:24:28 +0900

    Update TestManager.cs

--- Assets/Scripts/Managers/TestManager.cs
+++ Assets/Scripts/Managers/TestManager.cs
@@ -47,7 +47,7 @@ public class TestManager : MonoBehaviour
     [Header("Test UI")]
     [SerializeField] private GameObject testUI;
     [Space]
-    [SerializeField] private SliderConfig gameSpeed = new SliderConfig(1, 1, 20, "배속 × {0}");
+    [SerializeField] private SliderConfig gameSpeed = new SliderConfig(1, 1, 10, "배속 × {0}");
     [Space]
     [SerializeField] private TextMeshProUGUI testCountNum;
     [SerializeField] private TextMeshProUGUI maxScoreNum;
@@ -399,8 +399,6 @@ public class TestManager : MonoBehaviour
         maxScore = 0;
         totalScore = 0;
         averageScore = 0;
-
-        gameSpeed.value = gameSpeed.minValue;
 
         UpdateTestUI();
     }
